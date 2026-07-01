b7b6b066d6dc362a82a25c2bb0fa9bbce92c7737
dijanaliveric <dijanaliveric@gmail.com>
2026-07-01 14:34:41 +0200

    link na katastar - izmjene

--- app.py
+++ app.py
@@ -65,8 +65,8 @@ with glavni_col1:
     st.markdown("### 🧭 Navigacija")
     izbor = st.radio("Odaberite odjeljak:", popis_opcija, label_visibility="collapsed")
     st.write("---")
-    st.markdown("### 🔗 Provjeri česticu na službenom katastru:")
-    st.markdown("[📍 Prikaži mapu i granice posjeda](https://oss.uredjenazemlja.hr/map)")
+    st.markdown("### 🗺️ Točnu poziciju čestice na karti pogledajte ovdje")
+    st.markdown("[🌍 Geoportal Katastra (Uređena Zemlja)](https://oss.uredjenazemlja.hr/map)")
 
 
 # =========================================================================
